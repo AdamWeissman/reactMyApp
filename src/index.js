cfee6a623a1d3af3a3dfe332edeaee507d2e1a1d
AdamWeissman <adam@comedytragedyepic.com>
2020-07-14 14:23:20 -0400

    components lesson complete... next branch will be class components

--- src/index.js
+++ src/index.js
@@ -9,26 +9,41 @@ import faker from 'faker';
 
 const App = () => {
   return (
+
+
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail 
-        writer="Sam"
-        timeAgo="Today at 4:45PM"
-        content="NICE!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
+
+      <ApprovalCard>
+        <h4>WARNING</h4>
+        <div>Are  you sure you want to do this?</div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail 
+          writer="Sam"
+          timeAgo="Today at 4:45PM"
+          content="NICE!"
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
         writer="Alex"
         timeAgo="Today at 2:35PM"
         content="Cool post!"
         avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        writer="Jane"
-        timeAgo="Today at 1:15PM"
-        content="Locals can't believe this car insurance secret."
-        avatar={faker.image.avatar()} 
-      />
+       />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail
+          writer="Jane"
+          timeAgo="Today at 1:15PM"
+          content="Locals can't believe this car insurance secret."
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
     </div>  
   );
 };
